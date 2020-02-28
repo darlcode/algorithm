be508e991538f6623e12d3ffab0f9cac2c712777
darlcode <61573968+darlcode@users.noreply.github.com>
2020-02-28 20:54:08 +0900

    Create 백준_2502번_떡먹는호랑이.py

---
+++ baekjoon_algorithm/백준_2502번_떡먹는호랑이.py
@@ -0,0 +1,21 @@
+########### link : https://www.acmicpc.net/problem/2502 ##########
+## 2019.12.30 ##
+
+D, K = map(int, input().split())
+
+def fibo(n):
+  if n <= 1:
+    return 1
+  return fibo(n-1) + fibo(n-2)
+
+coefA = fibo(D-3)
+coefB = fibo(D-2)
+
+for A in range(K//coefA):
+    for B in range(K//coefB):
+        answer = coefA*A + coefB*B
+        if answer == K:
+            if A < B:
+                print(A)
+                print(B)
+                break
