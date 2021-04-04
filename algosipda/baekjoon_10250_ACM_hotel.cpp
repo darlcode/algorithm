d282f12186c91f74d0edb873f60ac457c4f08281
darlcode <rojelee@gmail.com>
2021-04-04 15:32:46 +0900

    add algosipda

---
+++ algosipda/baekjoon_10250_ACM_hotel.cpp
@@ -0,0 +1,41 @@
+// 백준 10250번
+// N번째 손님의 호텔 호수 구하기
+/*
+https://www.acmicpc.net/problem/10250
+*/
+
+#include <iostream>
+
+int main() 
+{
+    // 변수 목록
+    int testCase;
+    int floor;
+    int rooms;
+    int NstGuest;
+    int roomNumber = 0;
+    int quotient = 0;
+    int remainder = 0;
+
+    std::cin >> testCase;
+    for (int i = 0 ; i < testCase; i++)
+    {
+        std::cin >> floor >> rooms >> NstGuest;
+
+        // 나머지와 몫 계산
+        quotient = NstGuest/floor;
+        remainder = NstGuest%floor;
+
+        // 나머지가 0인 경우 floor를 1로 할당해줌
+        if(remainder == 0)
+            remainder = floor;
+
+        // 나머지 = 층 수, 몫 = 해당 층의 방 번호
+        roomNumber = (remainder * 100) + (quotient + 1);
+
+        std::cout << roomNumber << std::endl;
+    }
+    
+
+    return 0;
+}
